5d808a739c7335a4bfbad80e4edd9a1bc0cc1f64
itsDipsy <elliot.collinszcv@gmail.com>
2023-07-26 22:38:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -71,13 +71,125 @@ function input_check_for_yatzi_numbers_round(the_rounds_generated_numbers ,playe
     return the_pop_off_array;
 }
 
-function points_algorithm(){
+function points_algorithm(your_choosen_values_storage){
+    // klar
+    function check_for_dubble(){
+        for (let i = 0; i < your_choosen_values_storage.length; i++) {
+            let each_index = your_choosen_values_storage[i];
+            
+            for (let j = i + 1; j < your_choosen_values_storage.length; j++) {
+                let the_check = your_choosen_values_storage[j];
+                if(each_index === the_check){
+                    return 2;
+                }
+            }   
+        }
+        return false; // finns för att man ska veta att om den inte retunerar poängen så får man false
+    }
+    // klar
+    function check_for_tripple(){
+        let points_counter = 0;
+        for (let i = 0; i < your_choosen_values_storage.length; i++) {
+            let each_index = your_choosen_values_storage[i];
+            
+            for (let j = i + 1; j < your_choosen_values_storage.length; j++) {
+                let the_check = your_choosen_values_storage[j];
+                if(each_index === the_check){
+                    points_counter++;
+                }
+                if(points_counter === 3){
+                    return 3;
+                }
+            }   
+        }
+        return false; // finns för att man ska veta att om den inte retunerar poängen så får man false
+    }
+    // klar
+    function check_for_quad(){
+        let points_counter = 0;
+        for (let i = 0; i < your_choosen_values_storage.length; i++) {
+            let each_index = your_choosen_values_storage[i];
+            
+            for (let j = i + 1; j < your_choosen_values_storage.length; j++) {
+                let the_check = your_choosen_values_storage[j];
+                if(each_index === the_check){
+                    points_counter++;
+                }
+                if(points_counter === 4){
+                    return 4;
+                }
+            }   
+        }
+
+        return false; // finns för att man ska veta att om den inte retunerar poängen så får man false
+    }
+    // klar
+    function check_for_little_straight(){
+        let points_counter = 0;
+        for (let i = 0; i < your_choosen_values_storage.length; i++) {
+            if(your_choosen_values_storage[i] === i){
+                points_counter += 2;
+            }
+            else{
+                return false; // finns för att man ska veta att om den inte retunerar poängen så får man false
+            }
+        }
+    }
+    // inte klar
+    function check_for_big_straight(){
+       for (let i = 0; i < your_choosen_values_storage.length; i++) {
+        
+        
+       } 
+
+       return false; // finns för att man ska veta att om den inte retunerar poängen så får man false
+    }
+    // inte klar
+    function check_for_kåk(){
+        let points_counter = 0;
+        for (let i = 0; i < your_choosen_values_storage.length; i++) {
+            let each_index = your_choosen_values_storage[i];
+            
+            for (let j = i + 1; j < your_choosen_values_storage.length; j++) {
+                let the_check = your_choosen_values_storage[j];
+                if(each_index === the_check){
+                    points_counter++;
+                }
+                if(points_counter === 5){
+                    return 12;
+                }
+            }   
+        } 
+
+        return false; // finns för att man ska veta att om den inte retunerar poängen så får man false
+    }
+    // klar
+    function check_for_yatzy(){
+        let points_counter = 0;
+        for (let i = 0; i < your_choosen_values_storage.length; i++) {
+            let each_index = your_choosen_values_storage[i];
+            
+            for (let j = i + 1; j < your_choosen_values_storage.length; j++) {
+                let the_check = your_choosen_values_storage[j];
+                if(each_index === the_check){
+                    points_counter++;
+                }
+                if(points_counter === 5){
+                    console.log("Yatzy!!!!");
+                    return 50;
+                }
+            }   
+        }
+        return false; // finns för att man ska veta att om den inte retunerar poängen så får man false
+    }
     
+    console.log(check_for_yatzy());
 }
 
-play_a_round(your_choosen_values_storage); // startar spelet
+// play_a_round(your_choosen_values_storage); // startar spelet
 
-
+let test_array = [2,2,2,2,2];
+points_algorithm(test_array);
 
 
 /*
